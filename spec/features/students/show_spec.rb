1b4ed4fb88ec0df542f07ed0746ac1ab72292213
Austin Moore <austinchristianmoore@gmail.com>
2022-02-11 20:11:10 -0500

    User story 2

---
+++ spec/features/students/show_spec.rb
@@ -0,0 +1,27 @@
+# as a visitor,
+# When I visit '/students/:id'
+# I see a list of the students' courses.
+#
+# (e.g. "Defense against the Dark Arts"
+#       "Herbology"
+#       "Potions")
+require "rails_helper"
+
+RSpec.describe "students show page", type: :feature do
+  before(:each) do
+    @noel = Student.create!(name: 'Noel Sitnick', age: 15, house: 'Ravenclaw')
+    @austin = Student.create!(name: 'Austin Moore', age: 20, house: 'Gryffindor')
+    @dark_arts = @noel.courses.create!(name: "Defense against the Dark Arts" )
+    @herbology = @noel.courses.create!(name: "Herbology" )
+    @herbology = @austin.courses.create!(name: "Herbology" )
+    @potions = @austin.courses.create!(name: "Potions" )
+  end
+
+  scenario "visitor visits show page and sees list of students courses" do
+    visit "/students/#{@noel.id}"
+    expect(current_path).to eq("/students/#{@noel.id}")
+    expect(page).to have_content(@dark_arts.name)
+    expect(page).to have_content(@herbology.name)
+    expect(page).to_not have_content(@potions.name)
+  end
+end
